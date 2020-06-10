b46ba56e70d75b72b16521e8c72d31019637052f
Ирина Хон <khon.iv@mail.ru>
2020-06-10 16:27:34 +1000

    Улучшить интерфейс формы DBManager
    - настроить добавление всех TitledPane в AnchorPane в правую панель
    - ограничить высоту элемента Accordion
    - задать якори для левой и правой панели в основной SplitPane
    - задать отступы для левой и правой панели в основной SplitPane
    - убрать предупреждения IDE

--- src/main/java/controllers/DBManager/DBManagerController.java
+++ src/main/java/controllers/DBManager/DBManagerController.java
@@ -4,9 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TitledPane;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,46 +24,52 @@ public class DBManagerController implements Initializable {
     @FXML
     private ListView<String> teachers;
 
-    @FXML
-    private SplitPane mainPane;
-
-    @FXML public void handleMouseClickWeeks(MouseEvent event) {
+    @FXML public void handleMouseClickWeeks() {
              setRightPane("WeeksAmount");
     }
 
-    @FXML public void handleMouseClickGrades(MouseEvent event) {
+    @FXML public void handleMouseClickGrades() {
         String selectedItem = grades.getSelectionModel().getSelectedItem();
 
-        if (selectedItem == "Количество") {
-            setRightPane("GradesAmount");
-        } else if (selectedItem == "Нагрузка в неделю") {
-            setRightPane("GradesHoursPerWeek");
+        switch (selectedItem) {
+            case "Количество":
+                setRightPane("GradesAmount");
+                break;
+            case "Нагрузка в неделю":
+                setRightPane("GradesHoursPerWeek");
+                break;
         }
     }
 
-    @FXML public void handleMouseClickSubjects(MouseEvent event) {
+    @FXML public void handleMouseClickSubjects() {
         String selectedItem = subjects.getSelectionModel().getSelectedItem();
 
-        System.out.println("clicked on " + selectedItem);
-
-        if (selectedItem == "Название") {
-            setRightPane("SubjectsNames");
-        } else if (selectedItem == "Нагрузка в неделю") {
-            setRightPane("SubjectsHoursPerWeek");
-        } else if (selectedItem == "Нагрузка в день") {
-            setRightPane("SubjectsHoursPerDay");
+        switch (selectedItem) {
+            case "Название":
+                setRightPane("SubjectsNames");
+                break;
+            case "Нагрузка в неделю":
+                setRightPane("SubjectsHoursPerWeek");
+                break;
+            case "Нагрузка в день":
+                setRightPane("SubjectsHoursPerDay");
+                break;
         }
     }
 
-    @FXML public void handleMouseClickTeachers(MouseEvent event) {
+    @FXML public void handleMouseClickTeachers() {
         String selectedItem = teachers.getSelectionModel().getSelectedItem();
 
-        if (selectedItem == "Количество") {
-            setRightPane("TeachersAmount");
-        } else if (selectedItem == "Нагрузка в неделю") {
-            setRightPane("TeachersHoursPerWeek");
-        } else if (selectedItem == "Количество предметов") {
-            setRightPane("TeachersSubjects");
+        switch (selectedItem) {
+            case "Количество":
+                setRightPane("TeachersAmount");
+                break;
+            case "Нагрузка в неделю":
+                setRightPane("TeachersHoursPerWeek");
+                break;
+            case "Количество предметов":
+                setRightPane("TeachersSubjects");
+                break;
         }
     }
 
@@ -74,15 +79,21 @@ public class DBManagerController implements Initializable {
         setItems(grades, new String[]{"Количество", "Нагрузка в неделю"});
         setItems(subjects, new String[]{"Название", "Нагрузка в неделю", "Нагрузка в день"});
         setItems(teachers, new String[]{"Количество", "Нагрузка в неделю", "Количество предметов"});
-        setRightPane("WeeksAmount");
     }
 
     private void setItems(ListView<String> listView, String[] items) {
         listView.setItems(FXCollections.observableArrayList(items));
     }
 
+    @FXML
+    private AnchorPane rightPane;
+
     private void setRightPane(String fileName) {
         TitledPane titledPane = TitledPaneLoader.getTitledPane(fileName);
-        mainPane.getItems().set(1, titledPane);
+        rightPane.getChildren().clear();
+        AnchorPane.setLeftAnchor(titledPane,10.0);
+        AnchorPane.setRightAnchor(titledPane, 10.0);
+        AnchorPane.setTopAnchor(titledPane, 10.0);
+        rightPane.getChildren().addAll(titledPane);
     }
 }
